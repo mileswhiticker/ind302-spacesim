d8c3626fa72bda985583437e2dd33e55d52344cc
Miles Whiticker <miles.whiticker@gmail.com>
2012-11-28 21:17:28 +1000

    WIP trader stuff
    Signed-off-by: Miles Whiticker <miles.whiticker@gmail.com>

--- space_sim/Traders.cpp
+++ space_sim/Traders.cpp
@@ -1,1 +1,17 @@
 #include "Traders.hpp"
+
+#include <SFML\Graphics\Sprite.hpp>
+#include <SFML\Graphics\Texture.hpp>
+
+Trader::Trader()
+:	m_pCurrentLocation(NULL)
+,	m_Coords(sf::Vector2f())
+{
+	//
+}
+
+void Trader::JumpToLocation(DisplayableObject* a_pNewLocation, sf::Vector2f a_Coords)
+{
+	m_Coords = a_Coords;
+	m_pCurrentLocation = a_pNewLocation;
+}
--- space_sim/Traders.hpp
+++ space_sim/Traders.hpp
@@ -1,11 +1,26 @@
 #ifndef TRADERS_HPP
 #define TRADERS_HPP
 
+class DisplayableObject;
+#include <SFML\System\Vector2.hpp>
+namespace sf
+{
+	class Sprite;
+	class Texture;
+};
+
 class Trader
 {
 public:
+	Trader();
+	void JumpToLocation(DisplayableObject* a_pNewLocation, sf::Vector2f a_Coords = sf::Vector2f());
 	//
 private:
+	sf::Sprite* m_pSprite;
+	sf::Texture* m_pTexture;
+	//
+	DisplayableObject* m_pCurrentLocation;
+	sf::Vector2f m_Coords;
 	//
 };
 
